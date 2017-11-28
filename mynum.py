bc0281a078ade926b43987f9ad91e2cdd92f842a
Achilleas Ghinis <aghinis@berkeley.edu>
2017-11-28 10:36:27 -0800

    adding docstring

--- mynum.py
+++ mynum.py
@@ -1,7 +1,7 @@
 def mean(num_list):
-  first = num_list[0]
-  if isinstance(first,complex):
-    return NotImplemented
-  return sum(num_list)/len(num_list)
-
+    "Compute the mean of a list of numbers."
+    first = num_list[0]
+    if isinstance(first, complex):
+        return NotImplemented
+    return sum(num_list)/len(num_list)
 
